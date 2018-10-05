44bd1103cb270f09ac7406cc02d6dc681c78e41e
Patrick Boivin <pboivin@gmail.com>
2018-10-05 16:03:18 -0400

    Add CSS reset from bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 //import registerServiceWorker from './utils/registerServiceWorker';
 
+import 'bootstrap/dist/css/bootstrap-reboot.css';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
